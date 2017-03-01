b053bb4cc1cdb4b84ada93a91cb46c58a6de808c
roxblnfk <roxblnfk@mail.ru>
2017-03-01 12:19:24 +0300

    Compillation and building

--- _____COMPILLER.bat
+++ _____COMPILLER.bat
@@ -1,5 +1,9 @@
-:: (c) roxblnfk 2011
+:: (c) roxblnfk 2011-2017
 @echo off
 cd sources
 ".\php5.3.1\php.exe" -c ".\php5.3.1\php.ini" _____COMPILLER.php
+
+:: echo Копирование в билд
+:: cd ..\files\scripts
+:: copy *.phb ..\..\build\files\scripts
 pause
